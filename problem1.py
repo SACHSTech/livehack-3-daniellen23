072f24adf0353dcb5d6af6f97b55940f5685784d
daniellen23 <danielle.nguyen23@ycdsbk12.ca>
2021-03-07 04:51:48 +0000

    livehack 3

--- problem1.py
+++ problem1.py
@@ -1,1 +1,32 @@
 
+""""
+Name: TournamentTracker.py
+
+Purpose: This program will track the player wins and determine which group a player is placed in.
+
+Author: Nguyen.D 
+
+Created: 2021-03-04
+"""
+
+#get the player win and losses
+player = (input("Enter the wins and losses for your team: "))
+
+# Set the initial total
+total=0
+
+# count the number of wins or "w"
+for i in range(len(player)):
+  if player[i] == "w":
+    total = total + 1
+  print(player[i])
+
+# condition and print the team
+if total == 5 or total == 6:
+  print ("Your team is in Group 1")
+elif total == 3 or total == 4:
+  print ("Your team is in Group 2")
+elif total == 1 or total == 2:
+  print ("Your team is in Group 3")
+else:
+  print ("Your team is eliminated from the tournament")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,36 @@
+""""
+Name: round_distances.py
 
+Purpose: Round distances to the nearest kilometre.
+
+Author: Nguyen.D 
+
+Created: 2021-03-04
+"""
+# initilize counters
+distance =0
+total = 0
+count = 0
+
+# welcome header
+print("***** Welcome to the DoorDash Distance Tracker *****")
+print()
+
+# loop to get the distance per day
+while total < 100:
+  distance = int(input("Enter the distance travelled for the day: "))
+  total = total + distance
+  count += 1
+
+# calculate the speed  
+speed = total / count
+
+# print the days it took to surpass 200km 
+# and the average speed
+print()
+print("** Travel Log ** ")
+print("It took", count, "days to surpass 100km driven.")
+
+print()
+print (" ** Summary ** ")
+print("The average distance driven per day is ", int(speed), "km")
